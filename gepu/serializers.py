7aeaf612a537f2e7d0827c134cd9cfe952c5bdb6
Yoiyang <yal318@ucsd.edu>
2019-02-24 00:25:57 +0000

    pull request;

--- gepu/serializers.py
+++ gepu/serializers.py
@@ -1,30 +1,39 @@
-
 from rest_framework.serializers import ModelSerializer
 from .models import User, Group, Post, Event
 
-class UserSerializer(ModelSerializer):
+class UserInfoSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','phone','name', 'car_info', 'email','photo')
+
+class PostSerializer(ModelSerializer):
+    users = UserInfoSerializer(read_only=True, many=True)
+    class Meta:
+        model = Post
+        fields = ('id','eventID','users','isRide', 'third_Party', 'from_addr','count','time')
+
+class EventInfoSerializer(ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('id','title','to_addr','photo', 'info', 'createrID','posts','count','time')
+
+class GroupSerializer(ModelSerializer):
+    users = UserInfoSerializer(read_only=True, many=True)
+    events = EventInfoSerializer(read_only=True, many=True)
+    class Meta:
+        model = Group
+        fields = ('id','gid','name','admin', 'users', 'events')
+
+class EventSerializer(EventInfoSerializer):
+    group = GroupSerializer(read_only=True, many=False)
+    class Meta:
+        model = Event
+        fields = ('id','title','to_addr','photo', 'info', 'createrID','posts','count','time','groupID')
+
+class UserSerializer(UserInfoSerializer):
     groups = GroupSerializer(read_only=True, many=True)
     posts = PostSerializer(read_only=True, many=True)
     class Meta:
         model = User
         fields = ('id','phone','name', 'car_info', 'email','photo','groups','posts')
 
-class PostSerializer(ModelSerializer):
-    users = UserSerializer(read_only=True, many=True)
-    class Meta:
-        model = Post
-        fields = ('eventID','users','isRide', 'third_Party', 'from_addr','count','time')
-
-class EventSerializer(ModelSerializer):
-    # not really an id but a lead to the group
-    groupID = GroupSerializer(read_only=True, many=False)
-    class Meta:
-        model = Event
-        fields = ('id','title','to_addr','photo', 'info', 'createrID','posts','count','time','groupID')
-
-class GroupSerializer(ModelSerializer):
-    users = UserSerializer(read_only=True, many=True)
-    events = EventSerializer(read_only=True, many=True)
-    class Meta:
-        model = Group
-        fields = ('id','gid','name','admin', 'users', 'events')
