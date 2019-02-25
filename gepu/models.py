efefcbadf7baad1f3ea4def7fdab48d9ffc850bd
YoiYang <yal318@ucsd.edu>
2019-02-24 21:19:28 -0800

    database schema redesigned

--- gepu/models.py
+++ gepu/models.py
@@ -5,32 +5,30 @@ from django.utils import timezone
 class User(models.Model):
     # let postgres make its own primary key
     uid = models.UUIDField(default=uuid4, editable=False)
-    phone = models.CharField(max_length=15,default='')
-    name = models.CharField(max_length=20,default='')
-    car_info = models.CharField(max_length=25,default='',blank=True)
-    email = models.CharField(max_length=25,default='',blank=True)
-    photo = models.TextField(default='',blank=True)
-    groups = models.ManyToManyField('Group', null=True,blank=True)
-    posts = models.ManyToManyField('Post', null=True,blank=True)
-    # join_date = models.DateTimeField('date published')
+    phone = models.CharField(max_length=15,blank=False)
+    name = models.CharField(max_length=20,blank=False)
+    car_info = models.CharField(max_length=25,blank=True)
+    email = models.CharField(max_length=25,blank=True)
+    photo = models.TextField(blank=True)
+    #groups = models.ManyToManyField('Group', blank=True, null=True, related_name='users')
+    # posts = models.ManyToManyField('Post', blank=True)
+    join_date = models.DateTimeField(default=timezone.now,editable=False)
     def __str__(self):
         return self.name
 
-
 class Post(models.Model):
     #uid = models.UUIDField(default=uuid4, editable=False)
-    #eventID = models.ForeignKey('Event', on_delete=models.CASCADE,default=-1)
-    eventID = models.CharField(max_length=36,default='')
-    # assume the FIRST user created this post
-    users = models.ManyToManyField('User',default=-1)
+    # cascade: if the event is deleted, this post will be deleted
+    event = models.ForeignKey('Event',on_delete=models.CASCADE, related_name='posts')
+    # stores the id who craeted it
+    creatorID = models.DecimalField(max_digits=10, decimal_places=0)
     # indicator whether it's a ride request (True) or drive offer (False)
     isRide = models.BooleanField(default=False)
-    # indicator whther it's a ride share post that uses third party apps
     third_Party = models.BooleanField(default=False)
-    from_addr = models.TextField(default='')
-    # how many available spots (type 0, 2), how many people requests (type 1)
-    count = models.IntegerField(default=0)
+    from_addr = models.TextField(blank=True)
+    seats = models.PositiveIntegerField(default=0)
     time = models.DateTimeField(default=timezone.now)
+    users = models.ManyToManyField('User',default=-1, related_name='posts')
     def __str__(self):
         return str(self.time)
 
@@ -40,14 +38,12 @@ class Event(models.Model):
     to_addr = models.TextField(default='')
     photo = models.TextField(default='', null=True,blank=True)
     info = models.TextField(default='', null=True,blank=True)
-    #created = models.ForeignKey('User', on_delete=models.CASCADE, default=0)
-    createrID = models.CharField(max_length=36,default='')
-    posts = models.ManyToManyField('Post',default=-1,blank=True)
+    #posts = models.ManyToManyField('Post',default=-1,blank=True)
     # counter for claimed rides
     #count = models.IntegerField(default=0)
     time = models.DateTimeField(default=timezone.now)
     # cascade: if the group it belongs is deleted, it gets deleted.
-    groupID = models.ForeignKey('Group',on_delete=models.CASCADE)
+    group = models.ForeignKey('Group',on_delete=models.CASCADE, related_name='events')
     def __str__(self):
         return self.title
 
@@ -55,7 +51,7 @@ class Group(models.Model):
     gid = models.UUIDField(default=uuid4, editable=False)
     name = models.CharField(max_length=36,default='')
     admin = models.CharField(max_length=36,default='') # stores admin's uid
-    users = models.ManyToManyField('User', null=True,default=-1,blank=True)
-    events = models.ManyToManyField('Event', null=True,default=-1,blank=True)
+    users = models.ManyToManyField('User', null=True,default=-1,related_name='groups')
+    #events = models.ManyToManyField('Event', null=True,default=-1,blank=True)
     def __str__(self):
         return self.name
--- gepu/serializers.py
+++ gepu/serializers.py
@@ -1,32 +1,24 @@
 from rest_framework.serializers import ModelSerializer
 from .models import User, Group, Post, Event
 
-class UserInfoSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id','phone','name', 'car_info', 'email','photo')
-
-class PostSerializer(ModelSerializer):
-    users = UserInfoSerializer(read_only=True, many=True)
-    class Meta:
-        model = Post
-        fields = ('id','eventID','users','isRide', 'third_Party', 'from_addr','count','time')
-
-class EventSerializer(ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ('id','title','to_addr','photo', 'info', 'createrID','posts','count','time','groupID')
-
-class GroupSerializer(ModelSerializer):
-    users = UserInfoSerializer(read_only=True, many=True)
-    events = EventSerializer(read_only=True, many=True)
-    class Meta:
-        model = Group
-        fields = ('id','gid','name','admin', 'users', 'events')
-
-class UserSerializer(UserInfoSerializer):
-    groups = GroupSerializer(read_only=True, many=True)
-    posts = PostSerializer(read_only=True, many=True)
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('id','phone','name', 'car_info', 'email','photo','groups','posts')
+
+class PostSerializer(ModelSerializer):
+    users = UserSerializer(read_only=True, many=True)
+    class Meta:
+        model = Post
+        fields = ('id','event','creatorID','isRide', 'third_Party', 'from_addr','seats','time','users')
+
+class GroupSerializer(ModelSerializer):
+    users = UserSerializer(read_only=True, many=True)
+    class Meta:
+        model = Group
+        fields = ('id','gid','name','admin', 'users', 'events')
+        
+class EventSerializer(ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('id','title','to_addr','photo', 'info','posts','time','group')
