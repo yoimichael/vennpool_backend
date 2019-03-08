dc4571cf394414fb102e4e91010a1b4128511b31
YoiYang <yal318@ucsd.edu>
2019-03-07 19:02:34 -0800

    db redesign

--- gepu/models.py
+++ gepu/models.py
@@ -5,7 +5,7 @@ from django.contrib.postgres.fields import ArrayField
 
 class User(models.Model):
     '''
-    backward relations: posts, your_posts,events
+    backward relations: posts, your_posts, events, your_events
     '''
     # default id: Serial for user
     car_info = models.CharField(max_length=25,blank=True)
@@ -30,14 +30,14 @@ class Post(models.Model):
     third_Party = models.BooleanField(default=False)
     from_addr = models.TextField(blank=True)
     to_addr = models.TextField(blank=True)
-    seats = models.PositiveSmallIntegerField(default=0)
+    seats = models.PositiveSmallIntegerField(default=0,blank=True)
     event = models.ForeignKey('Event',on_delete=models.CASCADE, related_name='posts')
     creator = models.ForeignKey('User',on_delete=models.CASCADE,related_name="your_posts")
-    time = models.DateTimeField(default=timezone.now)
+    time = models.DateTimeField(default=timezone.now,blank=True)
     users = models.ManyToManyField('User',default=-1, related_name='posts')
 
     def __str__(self):
-        return str(self.time + self.from_addr + self.to_addr)
+        return str(self.time)
 
 class Event(models.Model):
     '''
@@ -47,6 +47,7 @@ class Event(models.Model):
     fb_eid = models.BigIntegerField(null=True, blank=True)
     members = models.ManyToManyField('User',default=-1, related_name='events')
     # fb API may provide
+    hosts = models.ManyToManyField('User',default=-1, related_name='your_events')
     title = models.CharField(max_length=20,default='')
     to_addr = models.TextField(default='')
     time = models.DateTimeField(default=timezone.now)
@@ -57,14 +58,18 @@ class Event(models.Model):
         return self.title
 
 def five_days_valid():
+    '''
+    return five days from now()
+    '''
     return timezone.now() + timezone.timedelta(days=5)
 
 class Hash(models.Model):
     hash_code = models.CharField(primary_key = True, max_length=4,default='BEEF')
+    public = models.BooleanField(default=True,blank=False)
     whitelist = ArrayField(models.BigIntegerField(null=True, blank=True), null=True, blank=True)
-    valid = models.BooleanField(default=False)
+    #valid = models.BooleanField(default=False,blank=False)
     valid_util = models.DateTimeField(default=five_days_valid)
-    event = models.ForeignKey('Event',on_delete=models.CASCADE, related_name='hash')
+    event = models.ForeignKey('Event',on_delete=models.SET_NULL, related_name='hash',null=True, default = None)
 
     def __str__(self):
-        return self.hash_code + ":" + str(self.event)
+        return self.hash_code
--- gepu/serializers.py
+++ gepu/serializers.py
@@ -1,25 +1,28 @@
 from rest_framework.serializers import ModelSerializer
 from .models import User, Hash, Post, Event
 
-# serializers used for RESTful responses
-class EventSerializer(ModelSerializer):
-    members = UserSerializer(read_only=True, many=True)
-    class Meta:
-        model = Event
-        fields = ('id','fb_eid', 'title','to_addr','time','info','photo','posts','members','hash')
-
-class HashSerializer(ModelSerializer):
-    class Meta:
-        model = Hash
-        fields = ('hash_code','whitelist','valid','valid_until','event')
-
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('id','car_info','phone', 'events', 'fb_id','messenger_id','name', 'email','photo','join_date','posts','your_posts','events')
+        fields = ('id','car_info','phone', 'events', 'fb_id','messenger_id','name', 'email','photo','join_date',
+        'posts','your_posts','events')
 
 class PostSerializer(ModelSerializer):
     users = UserSerializer(read_only=True, many=True)
     class Meta:
         model = Post
-        fields = ('id','isRide', 'third_Party', 'from_addr','to_addr','seats','event','creator','time','users')
+        fields = ('id','isRide', 'third_Party', 'from_addr','to_addr','seats','event',
+        'creator','time','users')
+
+# serializers used for RESTful responses
+class EventSerializer(ModelSerializer):
+    members = UserSerializer(read_only=True, many=True)
+    class Meta:
+        model = Event
+        fields = ('id','fb_eid', 'title','to_addr','time','info','photo',
+        'posts','members','hash')
+
+class HashSerializer(ModelSerializer):
+    class Meta:
+        model = Hash
+        fields = ('hash_code','whitelist','valid','valid_until','event')
