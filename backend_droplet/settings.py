f2f5e256b053f661e098a833a5dd58b2cee7b5d4
YoiYang <yal318@ucsd.edu>
2019-05-12 15:17:34 -0700

    get events bug - getorcreate return is a tuple

--- backend_droplet/settings.py
+++ backend_droplet/settings.py
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGGING = {'version': 1,'disable_existing_loggers': False,'handlers': {'file': {'level': 'DEBUG','class': 'logging.FileHandler','filename': '/root/django-apps/debug.log',},},'loggers': {'django': {'handlers': ['file'],'level': 'DEBUG','propagate': True,},},}
+#LOGGING = {'version': 1,'disable_existing_loggers': False,'handlers': {'file': {'level': 'DEBUG','class': 'logging.FileHandler','filename': '/root/django-apps/debug.log',},},'loggers': {'django': {'handlers': ['file'],'level': 'DEBUG','propagate': True,},},}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
